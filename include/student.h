fc6db0769ef65803eae1f297630efc5430aa6bd1
Chinmay Shet <chishxd@gmail.com>
2025-07-18 14:17:22 +0530

    Added functions to list and update students

--- include/student.h
+++ include/student.h
@@ -1,17 +1,16 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
-typedef struct{
-    int roll_no;
-    char name[50];
-    int age;
-    float marks;
+typedef struct {
+  int roll_no;
+  char name[50];
+  int age;
+  float marks;
 } Student;
 
 void add_student(Student students[], int *count);
 void list_students(const Student students[], int count);
-int find_student_by_id(const Student students[], int count, int id);
 void update_student(Student students[], int count, int id);
 void delete_student(Student students[], int *count, int id);
 
-#endif //STUDENT_H
+#endif  // STUDENT_H
