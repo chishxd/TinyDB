cc3e2280f83b5ae7a2078856be5854ce065fdb85
VedantParte23 <hugstonalbert@gmail.com>
2025-07-19 20:43:44 +0530

    Completed the project through file based system

--- src/library.c
+++ src/library.c
@@ -14,8 +14,7 @@ int Add_Student() {
     printf("Error reading name.\n");
     return 1;
   }
-  newStudent.name[strcspn(newStudent.name, "\n")] =
-      '\0'; /*newStudent madhe enter la space ne replace*/
+  newStudent.name[strcspn(newStudent.name, "\n")] = '\0';
 
   printf("Enter Student ID (Roll number): \n");
   if (scanf("%d", &newStudent.ID) != 1) {
@@ -31,14 +30,17 @@ int Add_Student() {
   }
   clearInputBuffer();
 
-  student_info = fopen("stud_info.txt", "a");
+  student_info = fopen("stud_info.bin", "ab");
   if (student_info == NULL) {
     printf("File can't be opened! An error occurred!!\n");
     return 1;
   }
 
-  fprintf(student_info, "%s\t%d\t%c\n", newStudent.name, newStudent.ID,
-          newStudent.grade);
+  if (fwrite(&newStudent, sizeof(Student), 1, student_info) != 1) {
+    printf("Error saving data.\n");
+    fclose(student_info);
+    return 1;
+  }
   printf("Data saved successfully.\n");
 
   fclose(student_info);
@@ -46,10 +48,10 @@ int Add_Student() {
 }
 
 int Display_Student() {
-  FILE *student_info = NULL;
-  char line_buffer[256]; /* Temprarory Arry display karnya sathi*/
+  FILE *student_info;
+  Student currentStudent;
 
-  student_info = fopen("stud_info.txt", "r");
+  student_info = fopen("stud_info.bin", "rb");
 
   if (student_info == NULL) {
     printf("Error: No student data found or file could not be opened.\n");
@@ -61,13 +63,15 @@ int Display_Student() {
   printf("-----------------------------------\n");
 
   int students_found = 0;
-  while (fgets(line_buffer, sizeof(line_buffer), student_info) != NULL) {
-    printf("%s", line_buffer);
+
+  while (fread(&currentStudent, sizeof(Student), 1, student_info) == 1) {
+    printf("%-16s %-6d %c\n", currentStudent.name, currentStudent.ID,
+           currentStudent.grade);
     students_found = 1;
   }
 
   if (students_found == 0) {
-    printf("No students records found in the file.\n");
+    printf("No student records found in the file.\n");
   }
   printf("-----------------------------------\n\n");
 
@@ -76,31 +80,195 @@ int Display_Student() {
 }
 
 int Search_Student() {
-  int search_num;
+  Student find_student;
+  int found = 0, input_roll;
+
   FILE *student_info = NULL;
-  if (student_info == NULL)
-    printf("Error occoured during opening of file. Fuck off!! :(");
-  student_info = fopen("stud_info.txt", "r");
-  printf("Enter his roll Number:");
-  if (scanf(" %d", &search_num) != 1) {
-    printf("Invalid Input");
+
+  printf("Enter the roll no(ID) of the student:\n");
+  if (scanf(" %d", &input_roll) != 1) {
+    printf("Error Wrong input");
     return 1;
   }
   clearInputBuffer();
+
+  student_info = fopen("stud_info.bin", "rb");
+
+  if (student_info == NULL) {
+    printf("Error opening file!\n");
+    return 1;
+  }
+
+  while (fread(&find_student, sizeof(Student), 1, student_info) == 1) {
+    if (find_student.ID == input_roll) {
+      printf("\n---- Displaying Student ----\n");
+      printf("Name             ID     Grade\n");
+      printf("-----------------------------------\n");
+      printf("%-16s %-6d %c\n", find_student.name, find_student.ID,
+             find_student.grade);
+      printf("-----------------------------------\n\n");
+      found = 1;
+    }
+  }
+
+  if (found == 0) {
+    printf("No such student found with %d ID", &input_roll);
+  }
+
+  return 0;
 }
 
-void Update_list() { printf("Hello!!"); }
+int Update_list() {
+  Student find_student;
+  int found = 0;
+  int input_roll;
+  char option;
 
-void Delete_Record() {
   FILE *student_info = NULL;
-  student_info = fopen("stud_info.txt", "w");
+  FILE *temp_info = NULL;
+
+  printf("Enter the roll no(ID) of the student you want to update:\n");
+  if (scanf(" %d", &input_roll) != 1) {
+    printf("Error: Wrong input. Please enter a number.\n");
+    clearInputBuffer();
+    return 1;
+  }
+  clearInputBuffer();
+
+  student_info = fopen("stud_info.bin", "rb");
   if (student_info == NULL) {
-    printf("'File is already empty!\n");
-    return;
+    printf("Error opening student_info.bin for reading!\n");
+    return 1;
+  }
+
+  temp_info = fopen("temp_stud_info.bin", "wb");
+  if (temp_info == NULL) {
+    printf("Error opening temp_stud_info.bin for writing!\n");
+    fclose(student_info);
+    return 1;
+  }
+
+  while (fread(&find_student, sizeof(Student), 1, student_info) == 1) {
+    if (find_student.ID == input_roll) {
+      found = 1;
+      printf("\n---- Displaying Student ----\n");
+      printf("Name             ID     Grade\n");
+      printf("-----------------------------------\n");
+      printf("%-16s %-6d %c\n", find_student.name, find_student.ID,
+             find_student.grade);
+      printf("-----------------------------------\n\n");
+      printf("Is this the student you want to update?(y/n): \n");
+      scanf(" %c", &option);
+      clearInputBuffer();
+      char upoption = toupper(option);
+
+      if (upoption == 'Y') {
+        printf("Enter the new Name: \n");
+        if (fgets(find_student.name, sizeof(find_student.name), stdin) ==
+            NULL) {
+          printf("Error reading new name.\n");
+          fclose(student_info);
+          fclose(temp_info);
+          remove("temp_stud_info.bin");
+          return 1;
+        }
+        find_student.name[strcspn(find_student.name, "\n")] = '\0';
+
+        printf("Enter new Student ID (Roll number): \n");
+        if (scanf("%d", &find_student.ID) != 1) {
+          printf("Invalid ID. Please enter a number.\n");
+          clearInputBuffer();
+          fclose(student_info);
+          fclose(temp_info);
+          remove("temp_stud_info.bin");
+          return 1;
+        }
+        clearInputBuffer();
+
+        printf("Enter new student grade: \n");
+        if (scanf(" %c", &find_student.grade) != 1) {
+          printf("Invalid grade. Please enter a single character.\n");
+          clearInputBuffer();
+          fclose(student_info);
+          fclose(temp_info);
+          remove("temp_stud_info.bin");
+          return 1;
+        }
+        clearInputBuffer();
+
+        if (fwrite(&find_student, sizeof(Student), 1, temp_info) != 1) {
+          printf("Error writing updated data to temporary file.\n");
+          fclose(student_info);
+          fclose(temp_info);
+          remove("temp_stud_info.bin");
+          return 1;
+        }
+        printf("Student information updated in memory. Saving...\n");
+      } else if (upoption == 'N') {
+        if (fwrite(&find_student, sizeof(Student), 1, temp_info) != 1) {
+          printf("Error writing original data to temporary file.\n");
+          fclose(student_info);
+          fclose(temp_info);
+          remove("temp_stud_info.bin");
+          return 1;
+        }
+        break;
+      } else {
+        printf(
+            "Invalid option. Please enter 'y' or 'n'. Skipping update for this "
+            "student.\n");
+        if (fwrite(&find_student, sizeof(Student), 1, temp_info) != 1) {
+          printf("Error writing original data to temporary file.\n");
+          fclose(student_info);
+          fclose(temp_info);
+          remove("temp_stud_info.bin");
+          return 1;
+        }
+        continue;
+      }
+    } else {
+      if (fwrite(&find_student, sizeof(Student), 1, temp_info) != 1) {
+        printf("Error writing data to temporary file.\n");
+        fclose(student_info);
+        fclose(temp_info);
+        remove("temp_stud_info.bin");
+        return 1;
+      }
+    }
   }
 
   fclose(student_info);
+  fclose(temp_info);
+
+  if (found == 0) {
+    printf("No such student found with ID %d.\n", input_roll);
+    remove("temp_stud_info.bin");
+  } else {
+    if (remove("stud_info.bin") != 0) {
+      printf("Error deleting original file.\n");
+      return 1;
+    }
+    if (rename("temp_stud_info.bin", "stud_info.bin") != 0) {
+      printf("Error renaming temporary file.\n");
+      return 1;
+    }
+    printf("Student data updated successfully.\n");
+  }
+
   return 0;
+}
+
+int Delete_Record() {
+  FILE *student_info = fopen("stud_info.bin", "wb");
+  if (student_info == NULL) {
+    printf("Error opening file!");
+    return 1;
+  }
+
+  fclose(student_info);
+
+  printf("Data deleted succesfully\n");
+  printf("|--------------------------|\n\n");
 }
 
 void clearInputBuffer() {
--- src/main.c
+++ src/main.c
@@ -1,7 +1,8 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "library.h"
 
 /*
@@ -10,56 +11,52 @@ gcc main.c library.c -o student_app
 ./student_app
 */
 
-int main()
-{
-    int option;
-    printf("==== STUDENT MANAGEMENT SYSTEM ====\n");
+int main() {
+  int option;
+  printf("==== STUDENT MANAGEMENT SYSTEM ====\n");
 
-    do
-    {
-        printf("|-- Options --|\n");
-        printf("1. Add Student\n");
-        printf("2. Display All Student\n");
-        printf("3. Search Student by ID\n");
-        printf("4. Update Student Record\n");
-        printf("5. Delete All Student Records\n");
-        printf("6. Exit\n");
-        printf("Enter your choice: ");
+  do {
+    printf("\n|-- Options --|\n");
+    printf("1. Add Student\n");
+    printf("2. Display All Student\n");
+    printf("3. Search Student by ID\n");
+    printf("4. Update Student Record\n");
+    printf("5. Delete All Student Records\n");
+    printf("6. Exit\n");
+    printf("Enter your choice: ");
 
-        if (scanf(" %d", &option) != 1)
-        {
-            printf("Invalid input. Try again!\n");
-            clearInputBuffer();
-            option = 0;
-            continue;
-        }
-        clearInputBuffer();
+    if (scanf(" %d", &option) != 1) {
+      printf("Invalid input. Try again!\n");
+      clearInputBuffer();
+      option = 0;
+      continue;
+    }
+    clearInputBuffer();
 
-        switch (option)
-        {
-        case 1:
-            Add_Student();
-            break;
-        case 2:
-            Display_Student();
-            break;
-        case 3:
-            Search_Student();
-            break;
-        case 4:
-            Update_list();
-            break;
-        case 5:
-            Delete_Record();
-            break;
-        case 6:
-            printf("Thanks for your service. Have a nice day! :)\n");
-            break;
-        default:
-            printf("Invalid Input. Fuck Off!\n");
-            break;
-        }
-    } while (option != 6);
+    switch (option) {
+      case 1:
+        Add_Student();
+        break;
+      case 2:
+        Display_Student();
+        break;
+      case 3:
+        Search_Student();
+        break;
+      case 4:
+        Update_list();
+        break;
+      case 5:
+        Delete_Record();
+        break;
+      case 6:
+        printf("Thanks for your service. Have a nice day! :)\n");
+        break;
+      default:
+        printf("Invalid Input. Fuck Off!\n");
+        break;
+    }
+  } while (option != 6);
 
-    return 0;
+  return 0;
 }
